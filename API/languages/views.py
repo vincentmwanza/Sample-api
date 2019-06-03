d3cca9982dfc0e67a3f9c6f66fa4a8a8c9471139
vincentmwanza <vincentm591@gmail.com>
2019-06-03 19:43:13 +0300

    removed white space

--- API/languages/views.py
+++ API/languages/views.py
@@ -3,7 +3,6 @@ from rest_framework import viewsets
 from .models import Language
 from .Serializers import LanguageSerializer
 
-
 class LanguageView(viewsets.ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
